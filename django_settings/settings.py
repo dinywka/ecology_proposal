50ce97d5512927716c3b9cc5c4ddb43bccfccf43
Dina <73873857+dinywka@users.noreply.github.com>
2023-07-12 10:26:09 +0600

    update

--- django_settings/settings.py
+++ django_settings/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q%8sr3=v^@mk1j$=f_1bpi9fa=5a_sqexv)v6(8w!9dn7b)v=d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['dinywka.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
